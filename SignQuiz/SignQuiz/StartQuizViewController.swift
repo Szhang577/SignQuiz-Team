e80cc926f18992ed942fb3c05f0ca72d35527988
Szhang577 <szhang577@wisc.edu>
2019-10-19 12:48:21 -0500

    Update view page on button

--- SignQuiz/SignQuiz/StartQuizViewController.swift
+++ SignQuiz/SignQuiz/StartQuizViewController.swift
@@ -1,5 +1,5 @@
 //
-//  ViewController.swift
+//  StartQuizViewController.swift
 //  SignQuiz
 //
 //  Created by 张斯媛 on 10/19/19.
@@ -8,17 +8,10 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class StartQuizViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
-
-    //MARK: Actions
-    @IBAction func startQuizButton(_ sender: UIButton) {
-        performSegue(withIdentifier: "startQuiz", sender: self)
-    }
-    
 }
-
