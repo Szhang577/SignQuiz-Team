a4bc1c5bc3bb23f64c25501a5ba73787e51b9e20
YurenSUN <ysun299@wisc.edu>
2019-10-19 11:50:32 -0500

    update Question class, delete question_generator and quiz_generator

--- SignQuiz/SignQuiz/Question.swift
+++ SignQuiz/SignQuiz/Question.swift
@@ -8,19 +8,25 @@
 
 import Foundation
 
-class Question_generator{
-    var answer: String;
-    var aslPath: String; //path to directory asl images
+class Question{
+    var answer: String
+    var aslPath: String //path to directory asl images
     
     init(aslPath: String, wordMin: Int, wordMax: Int) {
         // set the aslpath to the filepath
         // randomly generate an answer
-        self.aslPath = aslPath;
-        self.answer = Randoms.randomString(ofLength: Randoms.randomInt(wordMin,wordMax));
+        self.aslPath = aslPath
+        self.answer = ""
+        
     }
     
     func get_question(){
-
+        // generate question with asl images correspondanding to answer
         
     }
+    
+    func check(input: String) -> Bool {
+        return input == answer
+    }
+    
 }
